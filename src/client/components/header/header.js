383cfcffe18f3da8dc152a16633b52aa4c4d42b7
Ganpatkakar <kakarganpat@gmail.com>
2019-10-10 14:35:50 +0530

    Add header component for further navbar design

---
+++ src/client/components/header/header.js
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>M-Rec application</div>
+    );
+  }
+}
+
+Header.propTypes = {};
+
+
+export default Header;
