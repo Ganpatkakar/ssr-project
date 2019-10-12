db3ae482a5feacba5a4eccf9ae45b93771520a10
Ganpatkakar <kakarganpat@gmail.com>
2019-10-12 14:21:49 +0530

    Include component footer

---
+++ src/client/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import "../../styles/header.css";
+
+function Footer () {
+
+    return (
+      <div className={"footer"}>
+        <div className="nav-bar-logo">
+          <img src={"https://www.mercari.com/static/icons/logo-white.svg"} alt="Mercari Logo"/>
+        </div>
+      </div>
+    );
+}
+
+
+export default Footer;
