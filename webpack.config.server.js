c9a05926913ea0c9b247d9c624ef1dcd2403e51e
Ganpatkakar <kakarganpat@gmail.com>
2019-10-12 15:08:37 +0530

    include css configuration to server to recognize server side style code

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   target: 'node',
@@ -13,6 +14,9 @@ module.exports = {
   externals: [nodeExternals()],
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
   ],
   module: {
     rules: [
@@ -26,6 +30,20 @@ module.exports = {
             publicPath: path.resolve('/assets')
           }
         }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: path.resolve(__dirname, 'public')
+            }
+          },
+          "css-loader"
+        ]
       },
       {
         test: /\.js$/,
