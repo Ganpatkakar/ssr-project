52bd50d77414fb6869b3fed36db90fa87a3cfd18
Ganpatkakar <kakarganpat@gmail.com>
2019-10-12 13:59:23 +0530

    include footer and style the page with containers

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,8 +1,10 @@
-import React, {Component} from 'react';
+import "../styles/style.css";
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import {renderRoutes} from 'react-router-config';
 
 import Header from "./header/header";
+import Footer from "./footer/footer";
 
 export class App extends Component {
   constructor(props) {
@@ -12,10 +14,15 @@ export class App extends Component {
   render() {
     const {route} = this.props;
     return (
-      <React.Fragment>
-        <Header/>
-        <div>{renderRoutes(route.routes)}</div>
-      </React.Fragment>
+      <Fragment>
+        <div className="page-container">
+          <div className="content-wrap">
+            <Header/>
+            <div className={"container"}>{renderRoutes(route.routes)}</div>
+          </div>
+          <Footer />
+        </div>
+      </Fragment>
     )
   }
 }
