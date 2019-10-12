94e897f0d1397e8d8c89928b7a4b4414ca103e86
Ganpatkakar <kakarganpat@gmail.com>
2019-10-12 14:38:10 +0530

    Remove css webpack configuration from server side webpack configs

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   target: 'node',
@@ -14,9 +13,6 @@ module.exports = {
   externals: [nodeExternals()],
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new MiniCssExtractPlugin({
-      filename: "style.css"
-    })
   ],
   module: {
     rules: [
@@ -30,20 +26,6 @@ module.exports = {
             publicPath: path.resolve('/assets')
           }
         }]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it use publicPath in webpackOptions.output
-              publicPath: path.resolve(__dirname, 'public')
-            }
-          },
-          "css-loader"
-        ]
       },
       {
         test: /\.js$/,
