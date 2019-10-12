7130fe44984f4001fe487dffc0eab6981d2276be
Ganpatkakar <kakarganpat@gmail.com>
2019-10-12 14:19:33 +0530

    Indentation fix

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -1,28 +1,29 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import commonReducer from "./reducers/commonReducer";
+
 const windowExist = typeof window === 'object';
 let initialState = {};
 
 let middleware = [thunk];
 
 let configStore = (initialState = {}) => {
-    if(windowExist) {
-        return createStore(commonReducer,
-                initialState,
-                compose(
-                    applyMiddleware(thunk),
-                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                )
-            );
-    } else {
-        return createStore(commonReducer,
-                initialState,
-                compose(
-                    applyMiddleware(thunk)
-                )
-            );
-    }
+  if (windowExist) {
+    return createStore(commonReducer,
+      initialState,
+      compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    );
+  } else {
+    return createStore(commonReducer,
+      initialState,
+      compose(
+        applyMiddleware(thunk)
+      )
+    );
+  }
 };
 
 export default configStore;
