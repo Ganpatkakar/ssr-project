3b9759dca235065cc8465f0ce221fd54a636a405
Ganpatkakar <kakarganpat@gmail.com>
2019-10-12 14:15:18 +0530

    Reducer updated with single item fetch and remove item details Indentation fix

--- src/client/redux/reducers/commonReducer.js
+++ src/client/redux/reducers/commonReducer.js
@@ -1,32 +1,42 @@
-import { enums } from "../enums";
+import {enums} from "../enums";
 
 export default function commonReducer(state = {}, action) {
-    switch (action.type) {
-        case enums.INITIAL_STATE:
-            return {
-                ...state,
-                number: 0,
-                ready: action.payload
-            };
-        case enums.TEST_STATE:
-            return {
-                ...state,
-                userData: action.payload
-            };
-        case enums.ITEMS_FETCH:
-            return {
-                ...state,
-                items_list: action.payload
-            };
-        case 'Fetch_Data':
-            return {
-                fetching: true
-            };
-        case 'Fetch_Data_Failed':
-            return {
-                fetching: "failed"
-            };
-        default:
-            return state
-    }
+  switch (action.type) {
+    case enums.INITIAL_STATE:
+      return {
+        ...state,
+        number: 0,
+        ready: action.payload
+      };
+    case enums.TEST_STATE:
+      return {
+        ...state,
+        userData: action.payload
+      };
+    case enums.ITEMS_FETCH:
+      return {
+        ...state,
+        items_list: action.payload
+      };
+    case enums.SINGLE_ITEM_FETCH:
+      return {
+        ...state,
+        item: action.payload
+      };
+    case enums.SINGLE_ITEM_Remove:
+      return {
+        ...state,
+        item: {}
+      };
+    case 'Fetch_Data':
+      return {
+        fetching: true
+      };
+    case 'Fetch_Data_Failed':
+      return {
+        fetching: "failed"
+      };
+    default:
+      return state
+  }
 }
