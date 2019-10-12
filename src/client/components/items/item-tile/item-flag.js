5d4a432a5f3d03a01b7e4c89c6c6287ce8c888e5
Ganpatkakar <kakarganpat@gmail.com>
2019-10-12 14:22:27 +0530

    Sold out flag implementation

---
+++ src/client/components/items/item-tile/item-flag.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RenderFlag() {
+  return (
+    <div className={"sold-out"}>
+      <div className={"sold-out-content"}>SOLD</div>
+    </div>
+  )
+}
+
+export default RenderFlag;
